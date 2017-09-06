b56b10e7530e51e3cf6d1be99a64b812259cf402
SGatInviewAds <grossman.stuart1@gmail.com>
2017-09-06 13:34:16 -0700

    lets ese

--- dapp/views/sources/js/home/home.js
+++ dapp/views/sources/js/home/home.js
@@ -33,6 +33,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
    var email = error.email;
    // The firebase.auth.AuthCredential type that was used.
    var credential = error.credential;
+   console.log(errorCode, errorMessage, email, credential)
    // ...
   });
 }
