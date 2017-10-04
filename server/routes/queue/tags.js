475bc8b1bc6eaacdbf963121b885ade6baf6ca78
SGatInviewAds <grossman.stuart1@gmail.com>
2017-10-04 12:17:18 -0700

    tags

--- server/routes/queue/tags.js
+++ server/routes/queue/tags.js
@@ -16,14 +16,55 @@ var db = admin.database();
 var tagsRef = admin.database().ref('/queue/addTags/');
 var queue = new Queue(tagsRef, function(data, progress, resolve, reject) {
   // console.log(data)
+    var tag = data.data
+
+    db.ref('tag/' + data.userId).once('value', function(snapshot){
+      var tagData = snapshot.val();
+      console.log(data);
+      if(!tagData){
+        db.ref('tag/' + data.userId).push({data: tag}).then(function(res){
+          resolve();
+        })
+      }
+      if(tagData){
+        if(tagData.length > 5){
+          resolve();
+        }
+        var count = 0;
+        for(var i in tagData){
+          count += 1;
+          if(count >= 5){
+            resolve();
+          }
+          var temp = tagData[i].data
+          console.log(temp, data.data);
+          if(temp == data.data){
+            resolve();
+          }
+          db.ref('tag/' + data.userId).push({data: tag}).then(function(res){
+            resolve();
+          })
+        }
+      }
+    })
+
+
+
+});
+var getTagsRef = admin.database().ref('/queue/getTags/');
+var queue = new Queue(getTagsRef, function(data, progress, resolve, reject) {
+  // console.log(data)
   var tag = data.data
 
-    db.ref('tag/' + data.userId).push({data: tag}).then(function(res){
-      resolve();
+    db.ref('tag/' + data.userId).once('value', function(snapshot){
+      if(snapshot.val()){
+        for(var i in snapshot.val()){
+          console.log(snapshot.val()[i])
+        }
+      }
     })
 
 });
-
 
 
 module.exports = router;
