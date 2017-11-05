d1ef1d4359b22195af2f1554480f81c2d3e1150f
SGatInviewAds <grossman.stuart1@gmail.com>
2017-11-04 18:46:15 -0700

    please work

--- dapp/views/sources/js/main.js
+++ dapp/views/sources/js/main.js
@@ -1,101 +1,444 @@
-/*------------------------------------------------------------------
- * Theme Name: Hostify Responsive Template
- * Theme URI: http://www.brandio.io/envato/spacehost
- * Author: Brandio
- * Author URI: http://www.brandio.io/
- * Description: A Bootstrap Responsive HTML5 Template
- * Version: 1.0
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * Bootstrap v3.3.7 (http://getbootstrap.com)
- * Copyright 2017 Brandio.
- -------------------------------------------------------------------*/
+/*
+* PHLY - Versatile Coming Soon Template
+* Build Date: October 2015
+* Last Update: September 2016
+* Author: Madeon08
+* Copyright (C) 2016 Madeon08
+* This is a premium product available exclusively here : http://themeforest.net/user/Madeon08/portfolio
+*/
 
-"use strict";
+/*  TABLE OF CONTENTS
+    ---------------------------
+    1. Loading / Opening
+    2. Action Buttons
+    3. Scroll plugins
+    4. Newsletter
+    5. PhotoSwipe Gallery Init
+*/
 
-// Add Slider functionality to the #testimonials section in the home page.
-var testimonialsSlider = $("#testimonials-slider","#testimonials");
-testimonialsSlider.slick({
-    dots: false,
-    arrows: true,
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1
-});
-// Add Slider functionality to the testimonials in the "Sign in" and "Sign out" pages.
-var miniTestimonialsSlider = $(".mini-testimonials-slider","#form-section");
-miniTestimonialsSlider.slick({
-    dots: true,
-    arrows: false,
-    infinite: false,
-    autoplay: true,
-    speed: 200
-});
-// Add Slider functionality to the info-slider in the about page.
-var infoSlider = $(".info-slider","#page-head");
-infoSlider.slick({
-    dots: true,
-    arrows: false,
-    infinite: false,
-    autoplay: true,
-    speed: 200
-});
-$(window).on("load", function() {
-    // Counter slider functions in "CUSTOM HOSTING PLAN" section on the homepage
-    var cPlan = $('#c-plan');
-    cPlan.slider({
-        tooltip: 'always'
-    });
-    cPlan.on("slide", function(e) {
-        $('.slider .tooltip-up','#custom-plan').text(e.value/20);
-    });
-    cPlan.value = cPlan.data("slider-value");
-    $('.slider .tooltip','#custom-plan').append('<div class="tooltip-up"></div>');
-    $('.slider .tooltip-up','#custom-plan').text(cPlan.value/20);
-    $('.slider .tooltip-inner','#custom-plan').attr("data-unit",cPlan.data("unit"));
-    $('.slider .tooltip-up','#custom-plan').attr("data-currency",cPlan.data("currency"));
-    
-    // Features Section click function
-    var featureIconHolder = $(".feature-icon-holder", "#features-links-holder");
-    
-    featureIconHolder.on("click",function(){
-        featureIconHolder.removeClass("opened");
-        $(this).addClass("opened");
-        $(".show-details","#features-holder").removeClass("show-details");
-        $(".feature-d"+$(this).data("id"), "#features-holder").addClass("show-details");
-    });
-    
-    // Fix #features-holder height in features section
-    var featuresHolder = $("#features-holder");
-    var featuresLinksHolder = $("#features-links-holder");
-    var featureBox = $(".show-details","#features-holder");
-    
-    featuresHolder.css("height",featureBox.height()+120);
-    featuresLinksHolder.css("height",featureBox.height()+120);
+/* ------------------------------------- */
+/* 1. Loading / Opening ................ */
+/* ------------------------------------- */
 
-    // Fix #features-holder height in features section
-    $(window).on("resize",function() {
-        featuresHolder.css("height",featureBox.height()+120);
-        featuresLinksHolder.css("height",featureBox.height()+120);
+$(window).load(function(){
+    "use strict";
+
+    setTimeout(function(){
+        $('#preloader').velocity({
+
+            opacity: "0",
+
+            complete: function(){
+                $("#loading").velocity("fadeOut", {
+                    duration: 1000,
+                    easing: [0.7,0,0.3,1],
+                }); 
+            }
+        })
+
+    },1000);
+
+    setTimeout(function(){
+        $('.global-overlay').velocity({
+
+            translateX : "100%",
+            opacity : "1",
+
+        },
+
+        {
+            duration: 1000,
+            easing: [0.7,0,0.3,1],
+        })
+        
+        $(".map-container").addClass("fadeInRight").removeClass('opacity-0');
+
+    },1000);
+
+    setTimeout(function(){
+        $('#left-side').velocity({
+
+            opacity : "1",
+
+            complete: function(){
+
+            setTimeout(function() {
+                $('.text-intro').each(function(i) {
+                    (function(self) {
+                        setTimeout(function() {
+                            $(self).addClass('animated-middle fadeInUp').removeClass('opacity-0');
+                        },(i*150)+150);
+                        })(this);
+                    });
+                }, 0);
+            }
+
+        },
+
+        {
+            duration: 1000,
+            easing: [0.7,0,0.3,1],
+        })
+        
+    },1600);
+
+})
+
+$(document).ready(function(){
+    "use strict";
+
+    /* ------------------------------------- */
+    /* 2. Action Buttons ................... */
+    /* ------------------------------------- */
+
+    $('a#open-more-info').on( "click", function() {
+        $(".overlay").toggleClass("skew-part");
+        $("#right-side").toggleClass("hide-right");
+        $("#close-more-info").toggleClass("hide-close");
+        $('.mCSB_scrollTools').toggleClass('mCSB_scrollTools-left');
+        setTimeout(function() {
+            $("#mcs_container").mCustomScrollbar("scrollTo", "#right-side",{
+                scrollInertia:500,
+                callbacks:false
+            });
+        }, 350);
+    });
+
+    $('button#close-more-info').on( "click", function() {
+        $(".overlay").addClass("skew-part");
+        $("#right-side").addClass("hide-right");
+        $("#close-more-info").addClass("hide-close");
+        $('.mCSB_scrollTools').removeClass('mCSB_scrollTools-left');
+        setTimeout(function() {
+            $("#mcs_container").mCustomScrollbar("scrollTo", "#right-side",{
+                scrollInertia:500,
+                callbacks:false
+            });
+        }, 350);
+    });
+
+    // Youtube Variant
+
+    $('.expand-player').on( "click", function() {
+
+        $('#left-side').velocity({
+
+            opacity: "0",
+
+            complete: function(){
+                $('.global-overlay').velocity({
+
+                    translateX : "-100%",
+                    opacity : "0",
+
+                },
+
+                {
+                    duration: 1000,
+                    easing: [0.7,0,0.3,1],
+                    delay: 500,
+                })
+            }
+        })
+    });
+
+    $('.compress-player').on( "click", function() {
+
+        $('.global-overlay').velocity({
+
+            translateX : "100%",
+            opacity : "1",
+
+        },
+
+        {
+            duration: 1000,
+            easing: [0.7,0,0.3,1],
+            delay: 0,
+
+            complete: function(){
+
+                $('#left-side').velocity({
+
+                    opacity: "1",
+
+                })
+
+            }
+        })
+    });
+
+    /* ------------------------------------- */
+    /* 3. Scroll plugins ................... */
+    /* ------------------------------------- */
+
+    $(function() {
+        $('body').bind('mousewheel', function(event) {
+          event.preventDefault();
+          var scrollTop = this.scrollTop;
+          this.scrollTop = (scrollTop + ((event.deltaY * event.deltaFactor) * -1));
+          //console.log(event.deltaY, event.deltaFactor, event.originalEvent.deltaMode, event.originalEvent.wheelDelta);
+        });
+    });
+
+    var ifTouchDevices = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|Windows Phone)/);
+
+    // ScrollBar on Desktop, not on Touch devices for a perfect ergonomy
+    function scrollbar(){
+
+        if (ifTouchDevices){
+            $('body').addClass('scroll-touch');
+
+            $('a#open-more-info').on( "click", function() {
+                event.preventDefault();
+                var target = "#" + this.getAttribute('data-target');
+                $('body').animate({
+                    scrollTop: $(target).offset().top
+                }, 500);
+            });
+        } 
+
+        else {
+            $('body').mCustomScrollbar({
+                scrollInertia: 150,
+                axis            :"y"
+            });  
+        }
+    }
+  
+    scrollbar();
+
+    // Tooltips used on YouTube buttons
+    if (window.matchMedia("(min-width: 1025px)").matches) { 
+            
+        $(function () { $("[data-toggle='tooltip']").tooltip(); });
+
+    }
+
+    /* ------------------------------------- */
+    /* 4. Newsletter ........................ */
+    /* ------------------------------------- */
+
+    $("#notifyMe").notifyMe();
+
+    (function() {
+
+        var dlgtrigger = document.querySelector( '[data-dialog]' ),
+            somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
+            dlg = new DialogFx( somedialog );
+
+        dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
+
+    })();
+
+    /* ------------------------------------- */
+    /* 5. PhotoSwipe Gallery Init .......... */
+    /* ------------------------------------- */
+
+    var initPhotoSwipeFromDOM = function(gallerySelector) {
+
+    // parse slide data (url, title, size ...) from DOM elements 
+    // (children of gallerySelector)
+    var parseThumbnailElements = function(el) {
+        var thumbElements = el.childNodes,
+            numNodes = thumbElements.length,
+            items = [],
+            figureEl,
+            linkEl,
+            size,
+            item;
+
+        for(var i = 0; i < numNodes; i++) {
+
+            figureEl = thumbElements[i]; // <figure> element
+
+            // include only element nodes 
+            if(figureEl.nodeType !== 1) {
+                continue;
+            }
+
+            linkEl = figureEl.children[0]; // <a> element
+
+            size = linkEl.getAttribute('data-size').split('x');
+
+            // create slide object
+            item = {
+                src: linkEl.getAttribute('href'),
+                w: parseInt(size[0], 10),
+                h: parseInt(size[1], 10)
+            };
+
+
+
+            if(figureEl.children.length > 1) {
+                // <figcaption> content
+                item.title = figureEl.children[1].innerHTML; 
+            }
+
+            if(linkEl.children.length > 0) {
+                // <img> thumbnail element, retrieving thumbnail url
+                item.msrc = linkEl.children[0].getAttribute('src');
+            } 
+
+            item.el = figureEl; // save link to element for getThumbBoundsFn
+            items.push(item);
+        }
+
+        return items;
+    };
+
+    // find nearest parent element
+    var closest = function closest(el, fn) {
+        return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+    };
+
+    // triggers when user clicks on thumbnail
+    var onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var eTarget = e.target || e.srcElement;
+
+        // find root element of slide
+        var clickedListItem = closest(eTarget, function(el) {
+            return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+        });
+
+        if(!clickedListItem) {
+            return;
+        }
+
+        // find index of clicked item by looping through all child nodes
+        // alternatively, you may define index via data- attribute
+        var clickedGallery = clickedListItem.parentNode,
+            childNodes = clickedListItem.parentNode.childNodes,
+            numChildNodes = childNodes.length,
+            nodeIndex = 0,
+            index;
+
+        for (var i = 0; i < numChildNodes; i++) {
+            if(childNodes[i].nodeType !== 1) { 
+                continue; 
+            }
+
+            if(childNodes[i] === clickedListItem) {
+                index = nodeIndex;
+                break;
+            }
+            nodeIndex++;
+        }
+
+
+
+        if(index >= 0) {
+            // open PhotoSwipe if valid index found
+            openPhotoSwipe( index, clickedGallery );
+        }
         return false;
-    });
-    
-    // Apps Section hover function
-    var appHolder = $(".app-icon-holder", "#apps");
-    
-    appHolder.on("mouseover",function(){
-        appHolder.removeClass("opened");
-        $(this).addClass("opened");
-        $(".show-details", "#apps").removeClass("show-details");
-        $(".app-details"+$(this).data("id"), "#apps").addClass("show-details");
-    });
-    
-    // More Info Section hover function
-    var infoLink = $(".info-link", "#more-info");
-    
-    infoLink.on("mouseover",function(){
-        infoLink.removeClass("opened");
-        $(this).addClass("opened");
-        $(".show-details", "#more-info").removeClass("show-details");
-        $(".info-d"+$(this).data("id"), "#more-info").addClass("show-details");
-    });
+    };
+
+    // parse picture index and gallery index from URL (#&pid=1&gid=2)
+    var photoswipeParseHash = function() {
+        var hash = window.location.hash.substring(1),
+        params = {};
+
+        if(hash.length < 5) {
+            return params;
+        }
+
+        var vars = hash.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            if(!vars[i]) {
+                continue;
+            }
+            var pair = vars[i].split('=');  
+            if(pair.length < 2) {
+                continue;
+            }           
+            params[pair[0]] = pair[1];
+        }
+
+        if(params.gid) {
+            params.gid = parseInt(params.gid, 10);
+        }
+
+        return params;
+    };
+
+    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+        var pswpElement = document.querySelectorAll('.pswp')[0],
+            gallery,
+            options,
+            items;
+
+        items = parseThumbnailElements(galleryElement);
+
+        // define options (if needed)
+        options = {
+
+            // define gallery index (for URL)
+            galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+            getThumbBoundsFn: function(index) {
+                // See Options -> getThumbBoundsFn section of documentation for more info
+                var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+                    pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+                    rect = thumbnail.getBoundingClientRect(); 
+
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+
+        };
+
+        // PhotoSwipe opened from URL
+        if(fromURL) {
+            if(options.galleryPIDs) {
+                // parse real index when custom PIDs are used 
+                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+                for(var j = 0; j < items.length; j++) {
+                    if(items[j].pid === index) {
+                        options.index = j;
+                        break;
+                    }
+                }
+            } else {
+                // in URL indexes start from 1
+                options.index = parseInt(index, 10) - 1;
+            }
+        } else {
+            options.index = parseInt(index, 10);
+        }
+
+        // exit if index not found
+        if( isNaN(options.index) ) {
+            return;
+        }
+
+        if(disableAnimation) {
+            options.showAnimationDuration = 0;
+        }
+
+        // Pass data to PhotoSwipe and initialize it
+        gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+    };
+
+    // loop through all gallery elements and bind events
+    var galleryElements = document.querySelectorAll( gallerySelector );
+
+    for(var i = 0, l = galleryElements.length; i < l; i++) {
+        galleryElements[i].setAttribute('data-pswp-uid', i+1);
+        galleryElements[i].onclick = onThumbnailsClick;
+    }
+
+    // Parse URL and open gallery if it contains #&pid=3&gid=1
+    var hashData = photoswipeParseHash();
+    if(hashData.pid && hashData.gid) {
+        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
+    }
+};
+
+// execute above function
+initPhotoSwipeFromDOM('.my-gallery');
+
 });
