d808a295ea8002b6acc3bc556d7c9bb849fdcf83
SGatInviewAds <grossman.stuart1@gmail.com>
2018-08-30 19:13:55 -0700

    more fixes

--- server/routes/queue/tags.js
+++ server/routes/queue/tags.js
@@ -5,9 +5,10 @@ var fs = require('fs');
 var Queue = require('firebase-queue');
 var admin = require('firebase-admin');
 var db = admin.database();
-
-
+var getPersonalDataRef = admin.database().ref('/queue/personalData/');
 var addTagsRef = admin.database().ref('/queue/addTags/');
+
+
 var queue = new Queue(addTagsRef, function(data, progress, resolve, reject) {
   db.ref('tags/' + data.userId).once('value', function(snapshot){
     if(snapshot.val()){
@@ -15,11 +16,14 @@ var queue = new Queue(addTagsRef, function(data, progress, resolve, reject) {
         if(data.tag === childSnapshot.val()){
           db.ref('tags/' + data.userId).child(childSnapshot.key).remove().then(function(res, err){
             // console.log('deleting data');
-
+            if(err){
+              reject();
+            }
             resolve();
           })
         }
       })
+      //Outside of loop!
       db.ref('tags/' + data.userId).push(data.tag).then(function(res, err){
         // console.log('searched Data for duplicate, none found, added tag')
         if(err){
@@ -41,31 +45,6 @@ var queue = new Queue(addTagsRef, function(data, progress, resolve, reject) {
 });
 
 
-
-var checkTagsRef = admin.database().ref('queue/checkTags/');
-var queue = new Queue(checkTagsRef, function(data, progress, resolve, reject) {
-  var tag = data.tag
-
-
-});
-
-// var getTagsRef = admin.database().ref('/queue/getTags/');
-// var queue = new Queue(getTagsRef, function(data, progress, resolve, reject) {
-//   // console.log(data)
-//   var tag = data.data
-//
-//     db.ref('tag/' + data.userId).once('value', function(snapshot){
-//       if(snapshot.val()){
-//         for(var i in snapshot.val()){
-//           console.log(snapshot.val()[i])
-//         }
-//       }
-//     })
-//
-// });
-
-
-var getPersonalDataRef = admin.database().ref('/queue/personalData/');
 var queue = new Queue(getPersonalDataRef, function(data, progress, resolve, reject) {
   var updateData = {};
   updateData[data.dataName] =  data.data;
@@ -75,8 +54,6 @@ var queue = new Queue(getPersonalDataRef, function(data, progress, resolve, reje
     }
     resolve();
   })
-
-
 });
 
 module.exports = router;
