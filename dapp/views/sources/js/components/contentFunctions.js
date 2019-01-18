02c1578c1b234b0a5784776bca03f95dcca4fa14
Stuart Grossman <grossman.stuart1@gmail.com>
2019-01-17 19:53:08 -0800

    update

--- dapp/views/sources/js/components/contentFunctions.js
+++ dapp/views/sources/js/components/contentFunctions.js
@@ -7,9 +7,8 @@ var contentFunctions = (function(userObject, firebaseDataBase){
   //Ideally this file will be refactored into several different files
   //Complex frontend logic in vanilla javascript is amazing
 
-  //Function to check what type of device page is being accessed on
   var isMobile = false; //initiate as false
-// device detection
+  // device detection
   if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
       || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
       isMobile = true;
@@ -60,7 +59,24 @@ var contentFunctions = (function(userObject, firebaseDataBase){
       }
     })
   }
+  //seperates logic for linking event listener on button
+  function createEventListener(videoStatus, contentElementLink, lookup){
+    contentElementLink.addEventListener('click', function(){
+      contentElementLink.setAttribute('data-toggle', 'modal');
 
+      if(videoStatus){
+        // console.log('hitting videoStatus generate logic')
+        //if user has already finished watching video content
+        // var videoStatus = true;
+        contentElementLink.setAttribute('data-target', '#' + lookup + 'ModalResponse');
+      }else{
+        //if not set this button to open start modal of content
+        contentElementLink.setAttribute('data-target', '#' + lookup + 'Modal');
+
+      }
+      contentElementLink.setAttribute('id', 'open' + lookup + 'Modal');
+    })
+  }
   var exitScreen = false;
   var tagEle = document.createElement('div');
   tagEle.classList.add('btn', 'smallTag');
@@ -83,7 +99,7 @@ var contentFunctions = (function(userObject, firebaseDataBase){
 
           alertCount += 1; //alert count
           var lookup = data.contentList[i].lookup
-          console.log(data.contentList[i].progress.video.status)
+          // console.log(data.contentList[i].progress.video.status)
           watchLoader(lookup) //calls watchLoader on the id of the content element, each item gets its own watcher
           //opens Database looking for the the original published item through its authors UID and its Content UID
           firebaseDataBase.ref('publishedContent/' + data.contentList[i].author)
@@ -91,26 +107,17 @@ var contentFunctions = (function(userObject, firebaseDataBase){
              //Opens all publishers content
              //If any content is there proceed
             if(contentData.val()){
-
               var publishedItem = contentData.val()[lookup]; //current item being selected. lookup is the contents specific UID
               var dataId = data.contentList[i].author;
               var contentElementLink = document.createElement('a');
               //creates button for opening the modal -- /publishedData/userId/ lookup
               //Uses the published item ID as the pointer in the elements own id
+              var videoStatus = data.contentList[i].progress.video.status
+              // console.log('this is the video Stauts', videoStatus)
 
-              contentElementLink.addEventListener('click', function(){
-                contentElementLink.setAttribute('data-toggle', 'modal');
-                if(data.contentList[i].progress.video.status){
-                  //if user has already finished watching video content
-                  contentElementLink.setAttribute('data-target', '#' + lookup + 'ModalResponse');
-                }else{
-                  //if not set this button to open start modal of content
-                  contentElementLink.setAttribute('data-target', '#' + lookup + 'Modal');
+              createEventListener(videoStatus, contentElementLink, lookup)
 
-                }
-                contentElementLink.setAttribute('id', 'open' + lookup + 'Modal');
-              })
-              // console.log(contentData.val()[lookup], lookup, data)
+              //Sets title of element
               contentElementLink.innerHTML = publishedItem.productInfo.title;
               //create new List item append contentElement
               var contentEle = document.createElement('li');
@@ -118,15 +125,60 @@ var contentFunctions = (function(userObject, firebaseDataBase){
 
               document.getElementById('contentHolder').appendChild(contentEle);
               //creates Modals for content
-              const currentContent = contentData.val()[lookup];
-              createContentWindows(currentContent, lookup, data);
+              var currentContent = contentData.val()[lookup];
+              if(videoStatus){
+                createResponseContentWindows(currentContent, lookup, data)
+              }else{
+                //generate first window for each content
+                createContentWindows(currentContent, lookup, data);
+
+              }
             }
           })
         }
       }
     })
   }
+  function createResponseContentWindows(data, id, contentData){
+    console.log('hitting')
+    var blankModal = document.getElementById('blankModalResponse').cloneNode(true);
+    blankModal.id = id + 'ModalResponse';
+    document.getElementById('outerModalHolder').appendChild(blankModal);
+    var modalBody = blankModal.childNodes[1].childNodes[1]
+    var currentContent = contentData.contentList[id];
 
+    console.log(modalBody, data)
+    var modalHeaderContent =
+    data.productInfo.title + '  |  '
+    + '<span style="font-size:12px">'
+    +  data.productInfo.info +'</span>' +  ' | '
+    + '<span style="color:#fb9678; opacity: 0.8"> '
+    + currentContent.payout +'</span>'
+    + '<span style="font-size:12px; color:#fb9678; opacity:0.75">'
+    + ' PIES' + '</span>';
+
+    modalBody.childNodes[1].children[1].childNodes[1].innerHTML = modalHeaderContent;
+    var tempTagEle = tagEle.cloneNode(true);
+    console.log('currentContent var', currentContent)
+    matchPercentage = '<span style="font-size:12px">'
+    + currentContent["match%"]
+    + '%' + ' Match' + '</span>';
+    tempTagEle.innerHTML = matchPercentage
+
+    tempTagEle.classList.add('btn-danger')
+
+    modalBody.children[1].appendChild(tempTagEle)
+    //sets tags and userData
+    setDataTags(currentContent.tags, modalBody);
+    setDataTags(currentContent.userData, modalBody);
+
+    //setting links
+    console.log(modalBody.children[2].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1])
+    modalBody.children[2].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].setAttribute('href', data.source.link);
+
+
+
+  }
   function createContentWindows(data, id, contentData){
     var blankModal = document.getElementById('blankModal').cloneNode(true);
     blankModal.id = id + 'Modal';
@@ -267,26 +319,11 @@ var contentFunctions = (function(userObject, firebaseDataBase){
   }
 
   function contentFinishedQueue(id, modalBody){
-    //sets up next window
-    // secondStagePopulate(id)
-    //linksButton to newly populated window
-    // document.getElementById('stageTwoStart').setAttribute('data-target', '#blankModalResponse' + id)
 
-    // data-target="#modalResponse"
-    // turns off display of video and playButton div
-    // modalBody.childNodes[7].children[0].style.display = 'none';
-    // modalBody.children[2].children[1].style.display = 'none';
-    //TRUN ON DISPLAY OF STAGE TWO
     exitFullScreenBrowser(); // exits fullscreen on all browsers
     //hits exit button on modal
     modalBody.childNodes[1].children[0].click();
-    //removes modal from the DOM
-    removeElement('open'+id+'Modal')
 
-
-    // if(isMobile){
-    //   document.getElementById('open'+id+'Modal').click(); //closes window on mobile and opens it again
-    // }
 
     exitScreen = true; // prevents eventListener on fullScreen from invoking
 
@@ -294,54 +331,25 @@ var contentFunctions = (function(userObject, firebaseDataBase){
     var queue = firebaseDataBase.ref('/queue/contentFinished/tasks');
      queue.push({'userId': userObject.uid, 'contentId': id, 'mobile': isMobile}).then(function(){
        setTimeout(function(){
-         //short wait for dom to update
-         // console.log('updating dom')
-         // document.getElementById('contentResponseHolder').style.display = 'block';
-         // document.getElementById('open' + id + 'Modal').click();
-         // document.getElementById('stageTwoStart').click();
-         secondStagePopulate(id);
+
+         var contentElementLink = document.getElementById('open'+id+'Modal');
+         contentElementLink.setAttribute('data-target', '#' + id + 'ModalResponse');
+
+         // secondStagePopulate(id);
+         openResponseModal(contentElementLink);
 
          //reopenModal
        },2500)
 
-
-       //starts server request to watch content
-       //check database for status of content if True
-       //hit function to build next modalBody
-       //stop loading window
        return;
      })
   }
 
-  function secondStagePopulate(id){
-    console.log('second stage launch')
-    // var blankModal = document.getElementById('blankModalResponse').cloneNode(true);
-    // blankModal.id = id + 'Modal'
-    // var modalBody = blankModal.childNodes[1].childNodes[1]
-    // //sets title percentage and payment ammount
-    // var modalHeaderContent = data.productInfo.title + '  |  '
-    // + '<span style="font-size:12px">'
-    // +  data.productInfo.info +'</span>' +  ' | '
-    // + '<span style="color:#fb9678; opacity: 0.8"> '
-    // + currentContent.payout +'</span>'
-    // + '<span style="font-size:12px; color:#fb9678; opacity:0.75">'
-    // + ' PIES' + '</span>';
-    //
-    // modalBody.childNodes[1].children[1].childNodes[1].innerHTML = modalHeaderContent;
-    // //setting match percentage
-    // var tempTagEle = tagEle.cloneNode(true);
-    //
-    // matchPercentage = '<span style="font-size:12px">'
-    // + currentContent["match%"]
-    // + '%' + ' Match' + '</span>';
-    // tempTagEle.innerHTML = matchPercentage
-    //
-    // tempTagEle.classList.add('btn-danger')
-    //
-    // modalBody.children[1].appendChild(tempTagEle)
-    // //sets tags and userData
-    // setDataTags(currentContent.tags, modalBody);
-    // setDataTags(currentContent.userData, modalBody);
+  function openResponseModal(ele){
+    setTimeout(function(){
+      ele.click();
+
+    }, 500)
   }
 
   function seekerWatch(videoEle){
