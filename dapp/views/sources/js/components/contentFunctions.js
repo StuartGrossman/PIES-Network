627cc3a8a55b7ca32c1bfa8fc5a434bad88debef
Stuart Grossman <grossman.stuart1@gmail.com>
2019-01-18 00:56:42 -0800

    wow dat progress

--- dapp/views/sources/js/components/contentFunctions.js
+++ dapp/views/sources/js/components/contentFunctions.js
@@ -13,7 +13,7 @@ var contentFunctions = (function(userObject, firebaseDataBase){
       || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
       isMobile = true;
   }
-  console.log(isMobile)
+  // console.log(isMobile)
   //Function evaluates progress and hits loader() removes content if progress is not completed
   this.watchForLoad = function watchForLoad(){
     firebaseDataBase.ref('progressBar/' + userObject.uid).on('value', function(progressData){
@@ -65,9 +65,9 @@ var contentFunctions = (function(userObject, firebaseDataBase){
       contentElementLink.addEventListener('click', function(){
         firebaseDataBase.ref('content/' + userObject.uid + '/contentList/' + contentAddress).once('value', function(rawData){
           var videoStatus = rawData.val().progress.video.status
-          console.log(videoStatus, rawData.val())
+          // console.log(videoStatus, rawData.val())
           contentElementLink.setAttribute('data-toggle', 'modal');
-          console.log(videoStatus, 'happening again adding listener to ele')
+          // console.log(videoStatus, 'happening again adding listener to ele')
           if(videoStatus){
             // console.log('hitting videoStatus generate logic')
             //if user has already finished watching video content
@@ -145,14 +145,14 @@ var contentFunctions = (function(userObject, firebaseDataBase){
     })
   }
   function createResponseContentWindows(data, id, contentData){
-    console.log('hitting')
+    // console.log('hitting')
     var blankModal = document.getElementById('blankModalResponse').cloneNode(true);
     blankModal.id = id + 'ModalResponse';
     document.getElementById('outerModalHolder').appendChild(blankModal);
     var modalBody = blankModal.childNodes[1].childNodes[1]
     var currentContent = contentData.contentList[id];
 
-    console.log(modalBody, data)
+    // console.log(modalBody, data)
     var modalHeaderContent =
     data.productInfo.title + '  |  '
     + '<span style="font-size:12px">'
@@ -163,25 +163,25 @@ var contentFunctions = (function(userObject, firebaseDataBase){
     + ' PIES' + '</span>';
 
     modalBody.childNodes[1].children[1].childNodes[1].innerHTML = modalHeaderContent;
-    var tempTagEle = tagEle.cloneNode(true);
-    console.log('currentContent var', currentContent)
-    matchPercentage = '<span style="font-size:12px">'
-    + currentContent["match%"]
-    + '%' + ' Match' + '</span>';
-    tempTagEle.innerHTML = matchPercentage
 
-    tempTagEle.classList.add('btn-danger')
-
-    modalBody.children[1].appendChild(tempTagEle)
-    //sets tags and userData
-    setDataTags(currentContent.tags, modalBody);
-    setDataTags(currentContent.userData, modalBody);
 
     //setting links
-    console.log(modalBody.children[2].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1])
+    console.log(modalBody.children[2].childNodes[1].childNodes[11], data)
     modalBody.children[2].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].setAttribute('href', data.source.link);
 
+    //setting question and answer
+    modalBody.children[2].childNodes[1].childNodes[7].childNodes[1].childNodes[1].innerHTML = data.feedback.question;
 
+
+    var counter = 11
+    for(key in data.feedback.answers){
+      console.log(data.feedback.answers[key])
+
+      modalBody.children[2].childNodes[1].childNodes[counter].childNodes[1].childNodes[1].innerHTML = data.feedback.answers[key];
+      counter += 2
+    }
+
+    // modalBody.children[2].childNodes[1].childNodes[11].childNodes[1].innerHTML =
 
   }
   function createContentWindows(data, id, contentData){
@@ -228,33 +228,6 @@ var contentFunctions = (function(userObject, firebaseDataBase){
     //sets button event
     var modalButton = modalBody.childNodes[7].children[0].children[1].childNodes[1];
 
-    //Logic is checking state of content/progress
-    if((currentContent.progress.video.status === true) && (!currentContent.progress.link.status || !currentContent.progress.response.status)){
-      //hides inital elements
-      modalBody.childNodes[7].children[0].style.display = 'none';
-      modalBody.children[2].children[1].style.display = 'none';
-      modalBody.children[2].childNodes[1].style.display = 'none';
-      // document.getElementById('contentResponseHolder').style.display = 'blocked';
-      // showReponseWindow(id, modalBody);
-      // console.log('opening response')
-      //video completed
-      // if(currentContent.progress.link.status){
-      //   //link completed
-      //   if(currentContent.progress.response.status){
-      //     //response completed
-      //     if(!currentContent.progress.userAction.status){
-      //       // Checks last step if users has not submited
-      //       // This should always be false.
-      //     }
-      //   }
-      // }
-      // else{
-      //   //LINK LOGIC
-      // }
-    }
-    else{
-      //PLAY BUTTON IS HIT
-      //FRONT END MEAT LOGIC
 
       modalButton.addEventListener('click', function(){
         // modalBody.setAttribute('data-dismiss', "modal")
@@ -298,7 +271,7 @@ var contentFunctions = (function(userObject, firebaseDataBase){
           fullScreenWatcher(videoEle);
         }, 1000)
       })
-    }
+
   }
 
   function watchVideoTime(videoEle, internalTime, duration, id, modalBody, data, contentData){
