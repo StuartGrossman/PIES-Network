e69380c9dc861f8193d0d2df8d554f1bfad71c48
Stuart Grossman <grossman.stuart1@gmail.com>
2019-03-05 15:54:55 -0800

    update

--- views/sources/js/components/tagFunctions.js
+++ views/sources/js/components/tagFunctions.js
@@ -3,6 +3,7 @@ var tagFunctions = (function(userObject, firebaseDataBase){
   var buttonClickedCounter = 0;
   var tagReference = '/queue/addTags/tasks';
   checkTags();
+  checkBrandTags();
 
   this.addTag = function addTag(data){
     if(buttonClickedCounter == 0){
@@ -71,4 +72,65 @@ var tagFunctions = (function(userObject, firebaseDataBase){
       }
     })
   }
+
+  var tagReference = '/queue/addBrandTags/tasks';
+
+
+  this.addBrandTag = function addBrandTag(data){
+    if(buttonClickedCounter == 0){
+      //prevents over pressing
+      buttonClickedCounter += 1;
+      firebaseDataBase.ref(tagReference).push({'tag': data, 'userId': userObject.uid}).then(function(res){
+        if(document.getElementById(data)){
+          var ele = document.getElementById(data);
+          if(ele.classList.contains('btn-unpressed')){
+            removeClass(data);
+            buttonClickedCounter = 0;
+          }else{
+            addClass(data);
+            buttonClickedCounter = 0;
+          }
+        }
+      });
+    }
+  };
+
+  this.addCustomTag = function addCustomTag(data){
+    if(data.value.length > 1){
+       var data = data.value.split('');
+       var firstLetter = data[0].toUpperCase();
+       data.splice(0,1); data.unshift(firstLetter); data = data.join('');
+       firebaseDataBase.ref(tagReference).push({'tag': data, 'userId': userObject.uid}).then(function(res){
+         document.getElementById('tags').value = '';
+         return;
+       })
+    }
+  }
+
+
+  function checkBrandTags(){
+    firebaseDataBase.ref('/brandtags/' + userObject.uid).on('value', function(snapshot){
+      if(snapshot.val()){
+        snapshot.forEach(function(childSnapshot) {
+          var buttonHolder = document.getElementById(childSnapshot.val());
+          if(buttonHolder){
+              removeClass(childSnapshot.val())
+          }
+          else{
+            var newTagButtonEle = document.createElement('button');
+            newTagButtonEle.setAttribute("style", "margin-left: 6px; margin-bottom: 5px;");
+            newTagButtonEle.addEventListener('click', function(){
+              checkTagFunctions.addBrandTag(childSnapshot.val())
+            })
+            newTagButtonEle.classList.add('btn', 'btn-danger');
+            newTagButtonEle.id = childSnapshot.val();
+            newTagButtonEle.innerHTML = childSnapshot.val();
+            var tagHolder = document.getElementById('tagHolder');
+            tagHolder.prepend(newTagButtonEle);
+          }
+        })
+      }
+    })
+  }
+
 })
--- views/sources/js/components/typeWriterFunctions.js
+++ views/sources/js/components/typeWriterFunctions.js
@@ -2,7 +2,7 @@ var typeWriterFunctions = (function(){
   'use strict'
   var i = 0;
   var txt = ['Low fees. Fair exchange. Curated information.'
-  , 'Target your aduience customize your feedback.'
+  , 'Target your audience customize your feedback.'
   , "Get paid for you're precious time & make a impact with great feedback"]; /* The text */
   // , 'Target your Aduience, get real feedback.', 'Create better relationships between your content and its community.'
   var speed = 100; /* The speed/duration of the effect in milliseconds */
